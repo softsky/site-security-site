037b2927a80e1704ccdd05c12afb630999421afc
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-10-07 15:45:24 +0300

    major improvement of hashchange event and $.ajax action invokation

--- app/public/js/online-scan.js
+++ app/public/js/online-scan.js
@@ -1,5 +1,15 @@
+$(window).on('hashchange', () => {
+    if(window.location.hash){
+        var scanType = window.location.hash.match(/^#(.*)\/?/);
+        if(scanType && scanType.length > 1){
+            $('select#scanType').val(scanType[1]);
+        }
+    }
+
+});
 $(document).ready(() => {
     console.log('initialized');
+    $(window).trigger('hashchange');
     
     $('input#url').on('focus', () => {
 	$('.scan-result > dl').empty();
@@ -8,7 +18,12 @@ $(document).ready(() => {
     $('input#url').on('blur', (e) => {
 	const val = $(e.target).val()
 	, parser = document.createElement('a');
-	parser.href = val.startsWith('http:')?val:'http://' + val;
+	if(val.startsWith('http')){
+            parser.href = val;
+            $(e.target).val(val.replace(/https?:\/\//, ''));
+        } else {
+            parser.href = 'http://' + val;
+        }
 	const domain = parser.hostname.replace(/^www\./,'');
 
 	$('#domain').text('@' + domain);
@@ -51,33 +66,69 @@ $(document).ready(() => {
 	    $(e.target).val(val.replace(/\@.*/,''));
     });
     
-    $('button[type=submit]').click(() => {
-	console.log('Hiding');
-	$('form').fadeIn('slow', () => {
-	    $('h1').text('Thank you!');
-	});
+    $('button[type=submit]').click((e) => {
+        e.preventDefault();
+
+	// console.log('Hiding');
+	// $('form').fadeIn('slow', () => {
+	//     $('h1').text('Thank you!');
+	// });
 
 	var email = $('input#email').val();
 	if(email.indexOf('@') < 0){
 	    email += $('#domain').text();
 	};
 	
+	// $.ajax({
+	//     url: `/sendpulse/addressbooks/565640/emails`,
+	//     method: 'POST',
+	//     data: {
+        //         emails: JSON.stringify([{
+	// 	    email: email,
+	// 	    variables: {
+	// 	        firstName: $('input#firstName').val(),
+	// 	        lastName: $('input#lastName').val(),
+	// 	        website_url: $('input#url').val()
+	// 	    }
+	//         }])
+        //     },
+	//     success: (data, textStatus) => {
+	// 	console.log(data, textStatus);
+	//     }
+	// });
+
 	$.ajax({
-	    url: `/sendpulse/addressbooks/565640/emails`,
 	    method: 'POST',
-	    dataType: 'json',
-	    contentType: 'application/json',
-	    data: JSON.stringify([{
+	    url: `/api/on/online-scan/start`,
+            contentType: 'application/json',
+            dataType: 'json',
+	    data: JSON.stringify({
 		email: email,
-		variables: {
-		    firstName: $('input#firstName').val(),
-		    lastName: $('input#lastName').val(),
-		    website_url: $('input#url').val()
-		}
-	    }]),
+                name: {
+		    first: $('input#firstName').val(),
+		    last: $('input#lastName').val()                    
+                },
+                'scan-type': $('select#scanType').val(),
+                round: $('select#round').val(),
+                coupon: $('input#coupon').val(),
+		url: $('input#url').val()
+            }),
 	    success: (data, textStatus) => {
-		console.log(data, textStatus);
-	    }
+                const messages = {
+                    scheduled: 'Your scan has been scheduled. Please, check your mailbox',
+                    error: 'Error:' + data.msg + JSON.stringify(data.reason)
+                };
+		//console.log(data, textStatus);
+                $('.alert[role=alert]')
+                    .addClass('alert-success')
+                    .text(messages[data.status]);
+	    },
+            error: (err) => {
+                $('.alert[role=alert]')
+                    .addClass('alert-danger')
+                    .text(JSON.stringify(JSON.parse(err.responseText).error.message));
+            }            
 	});
+        return false;
     });
 });
