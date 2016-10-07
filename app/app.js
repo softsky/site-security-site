96696c1edd2a6c8e367529076acc37341ae37f26
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-10-07 15:44:20 +0300

    now rely on API_PORT env variable. PORT renamed to NODE_PORT

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-var PORT_LISTENER = process.env.PORT || 9000;
+var PORT_LISTENER = process.env.NODE_PORT || 9000;
 console.log('I am listening to this port: http://localhost:%s', PORT_LISTENER);
 
 var express = require('express'),
@@ -19,7 +19,8 @@ var express = require('express'),
 
 var appConfig = require('./config/appConfig.json');
 var router = require('./routes/index');
-
+var proxy = require('express-http-proxy');
+// 
 var app = express();
 
 // all environments
@@ -34,6 +35,16 @@ app.use(cookieParser('my v3ry s3cr3t C00k1e k3y d0nt y0u th1nk?'));
 app.use(session({ resave: true,
                   saveUninitialized: true,
                   secret: 'my l1ttl3 s3cret s3ss10n k3y isnt it?' }));
+// FIXME: currently we use 2 proxy libraries express-http-proxy and express-request-proxy
+// stick to oneo
+var [protocol, host, port] = (process.env.API_PORT || "tcp://localhost:3001").split(/\:/);
+host = host.replace(/\/\//, '');
+console.log('Host', host, 'Port', port);
+app.use('/api', proxy(`http://${host}:${port}`, {
+    forwardPath: function(req, res) {
+        return '/api' + require('url').parse(req.url).path;
+    }
+}));
 
 //routes
 app.use('/', router);
