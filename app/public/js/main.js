d586801dbdab658f94d7a9b07b177c4eff2a3a7b
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-10-12 21:04:57 +0300

    added main.js to landing page. Now `#clients a` links are opened in `_new` window

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(() => {
     console.log('ready');
+    // making all client links opening in a new window
+    $('#clients a').attr('target', '_new');
+    
     // TODO add some initialization stuff here
     $('img[src*=class]').each((idx, it) => {
         const clazz = $(it).attr('src').match(/class=([^$\&]*)/)[1];
@@ -7,10 +10,10 @@ $(document).ready(() => {
         console.log(clazz);
     });
 
-    $('#pricing a.btn').on('click',(e)=>{
-        var data = $(e.target).data();
-        data.action && $('#contact #ajax-form').attr('action', data.action);
-        data.subject && $('#contact input[name=subject]').val(data.subject);
-        data.message && $('#contact input[name=message]').val(data.message);
-    });
+    // $('#pricing a.btn').on('click',(e)=>{
+    //     var data = $(e.target).data();
+    //     data.action && $('#contact #ajax-form').attr('action', data.action);
+    //     data.subject && $('#contact input[name=subject]').val(data.subject);
+    //     data.message && $('#contact input[name=message]').val(data.message);
+    // });
 });
