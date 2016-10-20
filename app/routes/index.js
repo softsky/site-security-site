162a62c4533dfa1f423413e72b67080434deffe2
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-10-20 07:17:14 +0300

    minor change of array order

--- app/routes/index.js
+++ app/routes/index.js
@@ -141,7 +141,7 @@ module.exports = (function () {
         var id = req.params.id
         , section = req.params.section;
 
-        if(['css','img','media','images','fonts','js'].indexOf(section) > -1){
+        if(['css','img','images','media','fonts','js'].indexOf(section) > -1){
             next();
             return;
         }
